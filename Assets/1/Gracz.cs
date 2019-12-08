679fb782f42bbf284d5d4dec90ac9c4bb5f0c0ce
Filip <56913469+UnderMan4@users.noreply.github.com>
2019-12-08 22:33:57 +0100

    test

---
+++ Assets/1/Gracz.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gracz : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpHeight;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask WhatIsGround;
+    private bool grounded;
+    public bool doubleJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, WhatIsGround);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (grounded)
+        {
+            doubleJump = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            Jump();
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && !grounded && !doubleJump )
+        {
+            Jump();
+            doubleJump=true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+
+    void Jump()
+    { 
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, jumpHeight); 
+    }
+}
