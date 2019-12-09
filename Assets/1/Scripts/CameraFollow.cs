e29ef31149a8d9927440d3b2fecfac7c3ecdbd19
Filip <56913469+UnderMan4@users.noreply.github.com>
2019-12-09 21:55:01 +0100

    qqqqq
    paralax tło
    cegły po bokach
    skakanie
    kamera

---
+++ Assets/1/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    public float smoothSpped = 0.125f;
+
+   public void FixedUpdate()
+    {
+
+        Vector3 desiredPosition = target.position+offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpped);
+        transform.position = smoothedPosition;
+        if (transform.position.x < 0)
+        {
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > 0)
+        {
+            transform.position = new Vector3(0, transform.position.y, transform.position.z);
+        }
+    }
+}
