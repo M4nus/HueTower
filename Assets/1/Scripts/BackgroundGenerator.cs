45c129b2055568224347b27a14c4e7e19f3ade4f
Filip <56913469+UnderMan4@users.noreply.github.com>
2019-12-29 19:54:44 +0100

    dodatkowa scena dla loga naszego w 3d

--- Assets/1/Scripts/BackgroundGenerator.cs
+++ Assets/1/Scripts/BackgroundGenerator.cs
@@ -9,7 +9,7 @@ public class BackgroundGenerator : MonoBehaviour
     public Transform usedCamera;
     public float early = 2;
     float generationObjectHeight;
-    float usedCameraY;
+    //float usedCameraY;
     float lastGeneratedObjectPositionY;
     bool check = true;
 
@@ -23,15 +23,15 @@ public class BackgroundGenerator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        usedCameraY = usedCamera.position.y;
-        if (lastGeneratedObjectPositionY - usedCameraY <= generationObjectHeight * early)
+        //usedCameraY = usedCamera.position.y;
+        if (lastGeneratedObjectPositionY - gameObject.GetComponent<CameraScript>().usedCameraY <= generationObjectHeight * early)
         {
             Vector3 newPosition = new Vector3(startingObject.transform.position.x, lastGeneratedObjectPositionY + generationObjectHeight, startingObject.transform.position.z);
             Instantiate(generatingObject, newPosition, Quaternion.identity);
             lastGeneratedObjectPositionY = newPosition.y;
             check = false;
         }
-        if (usedCameraY + generationObjectHeight > lastGeneratedObjectPositionY * early)
+        if (gameObject.GetComponent<CameraScript>().usedCameraY + generationObjectHeight > lastGeneratedObjectPositionY * early)
             check = false;
         else
             check = true;
---
+++ Assets/1/Scripts/CameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform usedCamera;
+    public float usedCameraY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        usedCameraY = usedCamera.position.y;
+    }
+}
