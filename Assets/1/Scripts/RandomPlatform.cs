edcd318e6c146e2f9a6aee36930ed59fa078b7ac
Filip <56913469+UnderMan4@users.noreply.github.com>
2020-01-10 20:47:09 +0100

    Nareszcie działający skrypt losujacy platformy

--- Assets/1/Scripts/RandomPlatform.cs
+++ Assets/1/Scripts/RandomPlatform.cs
@@ -13,6 +13,7 @@ public class RandomPlatform : MonoBehaviour
     float usedCameraY;
     float generatingWidth;
     int generatingPlatformWidth;
+    int generatingplatformWidthFull;
     int[] generatingPlatformWidth2;
     int randomGeneratingPlatformCountPerFloor;
     int platformWidth;
@@ -28,42 +29,118 @@ public class RandomPlatform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastGeneratedObjectPositionY = -0.5f;
     }
+
+   
+    
+
 
     // Update is called once per frame
     void Update()
     {
         usedCameraY = usedCamera.position.y;
-
-        if (lastGeneratedObjectPositionY - usedCameraY <= generatingSpace * early)
+        randomGeneratingPlatformCountPerFloor = (int)Random.Range(1.0f, 5.9f);
+        if (lastGeneratedObjectPositionY - usedCameraY <= generatingSpace * early )
         {
-            randomGeneratingPlatformCountPerFloor = (int) Mathf.Floor(Random.Range(1.0f, 2.9f));
+            
             if(randomGeneratingPlatformCountPerFloor == 1)
             {
-                generatingPlatformWidth = (int) Mathf.Floor(Random.Range(5.0f, 10.0f));
-                randomPlatformsX = Random.Range(0.0f, 15.0f - generatingPlatformWidth) - 7;
+                generatingPlatformWidth = (int) Mathf.Floor(Random.Range(3.9f, 6.9f));
+                randomPlatformsX = Random.Range(0.0f, 15.0f - generatingPlatformWidth) - 9.5f;
 
                 Vector3 newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY, 0);
                 iterator = randomPlatformsX;
                 Instantiate(generatingL, newPosition, Quaternion.identity);
                 iterator = iterator + 1;
+                newPosition.x += 1;
 
                 for (int i = 0; i < (generatingPlatformWidth - 2); i++)
                 {
-                    newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY, 0);
+                    //newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY, 0);
                     Instantiate(generatingC, newPosition, Quaternion.identity);
-                    iterator = iterator + 1;
+                    //iterator = iterator + 1;
+                    newPosition.x += 1;
                 }
 
-                newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY, 0);
+                //newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY, 0);
                 Instantiate(generatingR, newPosition, Quaternion.identity);
-                iterator = iterator + 1;
-                lastGeneratedObjectPositionY = newPosition.y;
+                //iterator = iterator + 1;
+                newPosition.x += 1;
+                lastGeneratedObjectPositionY += generatingSpace;
                 check = false;
             }
-            else
+            else if (randomGeneratingPlatformCountPerFloor > 1)
             {
+                do
+                {
+                    generatingPlatformWidth = (int)Random.Range(7.1f, 19.9f);
+                    generatingPlatformWidth2[0] = (int)Random.Range(3.1f, 7.9f);
+                    generatingPlatformWidth2[2] = (int)Random.Range(3.1f, 7.9f);
+                }
+                while ((generatingPlatformWidth2[0] + generatingPlatformWidth2[2] + 3) <= generatingPlatformWidth);
+
+                generatingPlatformWidth2[1] = generatingPlatformWidth - generatingPlatformWidth2[0] - generatingPlatformWidth2[2];
+                Vector3 newPosition = new Vector3(randomPlatformsX, (lastGeneratedObjectPositionY + generatingSpace), 0);
+                randomPlatformsX = Random.Range(0, 18 - generatingPlatformWidth) - 9;
+                if (randomPlatformsX > -8.5f & randomPlatformsX < -7.5f)
+                {
+                    Instantiate(generatingC, newPosition, Quaternion.identity);
+                    newPosition.x += 1;
+                }
+                else
+                {
+                    Instantiate(generatingL, newPosition, Quaternion.identity);
+                    newPosition.x += 1;
+                }
+
+                for (int i = 0; i < (generatingPlatformWidth2[0] - 2); i++)
+                {
+                    Instantiate(generatingC, newPosition, Quaternion.identity);
+                    newPosition.x += 1;
+                }
+
+                Instantiate(generatingR, newPosition, Quaternion.identity);
+                newPosition.x += 1;
+
+                for (int i = 0; i < generatingPlatformWidth2[1]; i++)
+                {
+                    newPosition.x += 1;
+                }
+
+                Instantiate(generatingL, newPosition, Quaternion.identity);
+                newPosition.x += 1;
+
+                for (int i = 0; i < generatingPlatformWidth2[2] - 2; i++)
+                {
+                    Instantiate(generatingC, newPosition, Quaternion.identity);
+                    newPosition.x += 1;
+                }
+
+                if (newPosition.x > 7.5f & newPosition.x < 8.5f)
+                {
+                    Instantiate(generatingC, newPosition, Quaternion.identity);
+                }
+                else
+                {
+                    Instantiate(generatingR, newPosition, Quaternion.identity);
+                }
+                lastGeneratedObjectPositionY += generatingSpace;
+                check = false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+                /*
                 generatingPlatformWidth2 = new int[3];
                 generatingPlatformWidth2[0] = (int) Mathf.Floor(Random.Range(3.0f, 5.9f));
                 generatingPlatformWidth2[1] = (int) Mathf.Floor(Random.Range(6.9f - generatingPlatformWidth2[0], 9.9f - generatingPlatformWidth2[0]));
@@ -75,54 +152,64 @@ public class RandomPlatform : MonoBehaviour
                 if (randomPlatformsX < -7 & randomPlatformsX > -7.5f)
                 {
                     Instantiate(generatingC, newPosition, Quaternion.identity);
+                    newPosition.x += 1;
                 }
                 else
                 {
                     Instantiate(generatingL, newPosition, Quaternion.identity);
+                    newPosition.x += 1;
                 }
                 
                 for (int i = 0; i < (generatingPlatformWidth2[0] - 2); i++)
                 {
-                    newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
+                    //newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
                     Instantiate(generatingC, newPosition, Quaternion.identity);
-                    iterator = iterator + 1;
+                    //iterator = iterator + 1;
+                    newPosition.x += 1;
                 }
 
-                newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
+                //newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
                 Instantiate(generatingR, newPosition, Quaternion.identity);
-                iterator = iterator + 1;
+                //iterator = iterator + 1;
+                newPosition.x += 1;
 
                 for (int i = 0; i < generatingPlatformWidth2[1]; i++)
                 {
                     //newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
-                    iterator = iterator + 1;
+                    //iterator = iterator + 1;
+                    newPosition.x += 1;
                 }
 
-                newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
+                //newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
                 Instantiate(generatingL, newPosition, Quaternion.identity);
-                iterator = iterator + 1;
+                //iterator = iterator + 1;
+                newPosition.x += 1;
 
                 for (int i = 0; i < (generatingPlatformWidth2[2] - 2); i++)
                 {
-                    newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
+                    //newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
                     Instantiate(generatingC, newPosition, Quaternion.identity);
-                    iterator = iterator + 1;
+                    //iterator = iterator + 1;
+                    newPosition.x += 1;
                 }
 
                 if (16 - platformWidth < 1 & 16 - platformWidth > 0.5f)
                 {
-                    newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
+                    //newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
                     Instantiate(generatingC, newPosition, Quaternion.identity);
-                    iterator = iterator + 1;
+                    //iterator = iterator + 1;
+                    newPosition.x += 1;
                 }
                 else
                 {
-                    newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
+                    //newPosition = new Vector3(iterator + 1, lastGeneratedObjectPositionY + generatingSpace, 0);
                     Instantiate(generatingR, newPosition, Quaternion.identity);
-                    iterator = iterator + 1;
+                    //iterator = iterator + 1;
+                    newPosition.x += 1;
                 }
                 lastGeneratedObjectPositionY = newPosition.y;
                 check = false;
+                */
             }
             
         }
